04a4c83e76beb1065fef6f7daa3b6ee2efbd371e
stijngeysen <stijn.geysen@luminex.be>
2025-10-09 11:24:38 +0200

    Add eslint ignores to disable lint checking generated files or node modules'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,5 @@ import { generateEslintConfig } from '@companion-module/tools/eslint/config.mjs'
 
 export default generateEslintConfig({
 	enableTypescript: true,
+	ignores: ['dist/**', 'node_modules/**', '.yarn/**', '.pnp.cjs', '.pnp.loader.mjs'],
 })
